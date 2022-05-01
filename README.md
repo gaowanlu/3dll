45ef446e78c430ee448fd46be5c89f3f2af134df
gaowanlu <2209120827@qq.com>
2022-05-01 17:24:27 +0800

    支持 鼠标操控摄像机旋转、放大、缩小

--- README.md
+++ README.md
@@ -1,28 +1,53 @@
 # 3dll
 
-ûѧ�������ͼ��ѧ��ͼ�����γ̵Ĳ˼�������ܽ����Դ�����ͻȻ����һ�ȴ�����ͷ�� ���ߴ�������̫�����ˡ�  
-����ܹ���������Ĵ��¹���һ���������Ա任�����任���Լ�����������˵����ͼ������ϵ����������ϵ�����������ϵ��  
-�ⲻ������ĳ�������漰��Щ������������Ӿ���λSLAM�Լ�3D��������Ϸ������������Щ������أ���ǰ��Ҳ�Ӵ���RoboMaster�Ļ������Ӿ�����������ͼ������
-�������ҵ�һ�����ԣ����������Ȥ�Ļ����Գ���һ�£������ǳ���Ȥ�����ﲢû��ѡ��רҵ����Ⱦ���ߣ���OpenGL���Լ�ͼ������OpenCV��������һ���򵥵���������϶��֪ʶ������Ҫ�ġ�
-�ڴ�����ʹ��EasyX��Eigen3  
+使用C++实现简单的3D渲染
 
-## ���ٿ�ʼ
+## 快速开始
 
-��ʹ��Visual Studio����Ŀ  
+```shell
+git clone https://hub.fastgit.xyz/gaowanlu/3dll.git
+```
 
-��Ŀ =������ =��C/C++ =���������ӱ���Ŀ��include�ļ���·�������Ӱ���Ŀ¼����  
+## 为什么写了它
 
-��Ŀ =������ =�������� =��������Ŀ��lib/x64�ļ���·�����Ӹ��ӿ�Ŀ¼  
+没学过计算机图形学、图像处理课程的菜鸡最近在总结线性代数、突然脑子一热代码上头了 、线代的魅力太精彩了。  
+如果能够理解下面的大致过程一定会会对线性变换、基变换、以及可能我们听说过的图像坐标系、世界坐标系、摄像机坐标系。  
+这不仅仅在某个方向涉及这些，例如机器人视觉定位SLAM以及3D动画、游戏开发都会与这些密切相关，以前我也接触过RoboMaster的机器人视觉开发、做过图像处理，
+但这是我的一个尝试，如果你有兴趣的话可以尝试一下，我想会非常有趣，这里并没有选择专业的渲染工具，如OpenGL、以及图像处理的OpenCV，我想用一个简单的例子理解较多的知识是最重要的。
+在此我们使用EasyX与Eigen3  ，有人说为什么不去学OpenGL、我只能说术业有专攻我从没接触过他，我用简单的实现原理来帮助我理解线性代数、而非为了渲染效率工程专业性上考虑。
 
-ע���������沽��ʱ������ѡ�� ���ã��������� ƽ̨��x64  ������������ôŪ���ܲ�����  
+## 实现效果
 
-## ���Դ���
+1、相机坐标系的旋转
 
-�γ��Ƽ�  
+![在这里插入图片描述](https://img-blog.csdnimg.cn/5f74decc37d348549c864ae0291ad3c8.png#pic_center)
+2、鼠标点击且移动鼠标进行相机的旋转、滚轮滚动为z轴旋转
 
-* Bilibili MIT���Դ����γ� https://www.bilibili.com/video/BV1ix411f7Yp  
-* ���Դ����ı���  https://www.bilibili.com/video/BV1ib411t7YR   
+![在这里插入图片描述](https://img-blog.csdnimg.cn/401d1c84a9b444fb9a013f1523e09983.png#pic_center)
 
-## ·����  
+3、Ctrl+滚轮 支持缩小放大
 
-������ʱ���Ǿ���һ�ж���û��̫����;���������ǵ���֪ˮƽ����������ѧϰ��·�ϡ���ɽ��·��Ϊ����ѧ�����Ŀ����ۡ�
+![在这里插入图片描述](https://img-blog.csdnimg.cn/cdb8b27c00324e93a30b96435d765412.png#pic_center)
+
+## 环境搭建
+
+或在Github [https://github/gaowanlu/3dll](https://github/gaowanlu/3dll)、获取最新的工程效果  
+
+请使用Visual Studio 2022 打开项目  ，或者自己使用其他IDE进行构建  
+
+项目 =》属性 =》C/C++ =》常规添加本项目的include文件夹路径到附加包含目录属性  
+
+项目 =》属性 =》链接器 =》将本项目的lib/x64文件夹路径添加附加库目录  
+
+注意设置上面步骤时，尽量选择 配置：所有配置 平台：x64  、以免新手怎么弄都跑不起来  
+
+## 线性代数
+
+课程推荐  
+
+* Bilibili MIT线性代数课程 <https://www.bilibili.com/video/BV1ix411f7Yp>  
+* 线性代数的本质  <https://www.bilibili.com/video/BV1ib411t7YR>
+
+## 路漫漫  
+
+我们有时我们觉得一切东西没有太大用途、那是我们的认知水平还不够、在学习的路上、书山有路勤为径、学海无涯苦作舟。
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <cmath>
 #include <vector>
 #include <conio.h>
-#include <Eigen\Dense>
+#include <Eigen/Dense>
 #include "camera/camera.h"
 #define PI 3.14159265
 #define WINDOW_WITH 640
@@ -31,6 +31,16 @@ Eigen::Matrix<double, 3, 3> roate_z(double angle) {
 	return m;
 }
 
+std::vector<Eigen::Matrix<double, 3, 1>>  resize_model(std::vector<Eigen::Matrix<double, 3, 1>> model,double resize) {
+	for (auto& matrix : model) {
+		matrix(0) *= resize;
+		matrix(1) *= resize;
+		matrix(2) *= resize;
+	}
+	return model;
+}
+
+
 /*
 	此处的x，y为相机坐标系，原点在屏幕中心，右为x，上为y
 	esayx 坐标原点在设备的左上角，X 轴向右为正，Y 轴向下为正，度量单位是像素
@@ -39,8 +49,9 @@ Eigen::Matrix<double, 3, 3> roate_z(double angle) {
 Eigen::Matrix<double,3,1> to_xy(double x,double y,double z) {
 	//计算相机到目标的举例
 	double distance = sqrt(fabs(x) * fabs(x) + fabs(y) * fabs(y) + fabs(z) * fabs(z));
+	//std::cout << distance << std::endl;
 	double dot_size = 5;
-	double resize = 40;
+	double resize = 40;//400/fabs(z)
 	x *= resize;//缩放因数
 	y *= resize;
 	z *= resize;
@@ -101,12 +112,12 @@ void create_model_3(std::vector<Eigen::Matrix<double, 3, 1>>& model) {
 
 int main(int argc, char** argv) {
 	//建立窗口
-	initgraph(640, 480);	// 创建绘图窗口，大小为 640x480 像素
+	initgraph(1000, 700);	// 创建绘图窗口，大小为 640x480 像素
 	setlinecolor(RED);
 	//世界坐标系基
 	Eigen::Matrix<double, 3, 3> world_base;
 	world_base << 1., 0., 0., 0., 1., 0., 0., 0., 1.;
-	
+
 	//物品1
 	Eigen::Matrix<double, 3, 1> m1;
 	m1 << 0, 0, 5;
@@ -120,7 +131,7 @@ int main(int argc, char** argv) {
 	create_model_2(model2);//创建模型2
 	std::vector<Eigen::Matrix<double, 3, 1>>model3;
 	create_model_3(model3);//创建模型3
-	std::vector<std::vector<Eigen::Matrix<double, 3, 1>>>models{model1,model2};
+	std::vector<std::vector<Eigen::Matrix<double, 3, 1>>>models{ model1,model2 };
 
 	//摄像机 正交基 人在(0,0,0) 不动摄像机进行转动
 	Eigen::Matrix<double, 3, 3> camera;
@@ -128,45 +139,72 @@ int main(int argc, char** argv) {
 
 	//求世界坐标系下的坐标在摄像机坐标系表示的形式 基变换
 	//有 m*x=carama*y 求 y ，则y=carama逆*m*x 
-	std::cout <<"["<< camera.inverse() * world_base * m1 <<"]"<< std::endl;
-	std::cout <<"["<< camera.inverse() * world_base * m2 <<"]"<< std::endl;
+	std::cout << "[" << camera.inverse() * world_base * m1 << "]" << std::endl;
+	std::cout << "[" << camera.inverse() * world_base * m2 << "]" << std::endl;
 
 	//用三个角度表示摄像机在(0,0,0) x y z旋转的角度
 	double x_angle = 0, y_angle = 0, z_angle = 0;
-	std::cout << camera * roate_x(x_angle)* roate_y(y_angle) * roate_z(z_angle) << std::endl;
-	//尝试连续旋转
-	int count = 0;
-	double a = 0, b = 0, c = 0;
-	//for (; a < 180; a+=0.5) {
-	//	for (; b < 180; b+=0.5) {
-			Eigen::Matrix<double, 3, 3> camera_realtime;
-			for (c=0,b=74,a=-30; c < 360; c+=0.1,b+=0.1,b+=0.1) {
-				count++;
-				//x_angle = a, y_angle = b, z_angle = c;
-				x_angle = c, y_angle = b, z_angle = 0;
-				//取出相机的向量进行基变换
-				camera_realtime = roate_x(x_angle)* roate_y(y_angle)* roate_z(z_angle)*camera;//实时相机
-				for (auto& model : models) {
-					//渲染model
-					for (std::size_t i = 0; i < model.size(); i++) {
-						Eigen::Matrix<double, 3, 1> dot = model[i];
-						Eigen::Matrix<double, 3, 1> dot1_realtime = camera_realtime.inverse() * world_base * dot;
-						for (std::size_t j = 0; j < model.size(); j++) {
-							Eigen::Matrix<double, 3, 1> dot = model[j];
-							Eigen::Matrix<double, 3, 1> dot2_realtime = camera_realtime.inverse() * world_base * dot;
-							Eigen::Matrix<double, 3, 1> window_dot1 = to_xy(dot1_realtime(0), dot1_realtime(1), dot1_realtime(2));
-							Eigen::Matrix<double, 3, 1> window_dot2 = to_xy(dot2_realtime(0), dot2_realtime(1), dot2_realtime(2));
-							draw_t_line(window_dot1, window_dot2);
-						}
+	std::cout << camera * roate_x(x_angle) * roate_y(y_angle) * roate_z(z_angle) << std::endl;
+	//记录上一次鼠标的位置
+	int last_x = 0, last_y = 0;
+	double last_resize = 1.0;
+
+	Eigen::Matrix<double, 3, 3> camera_realtime;
+
+	while (1) {
+		//取出相机的向量进行基变换
+		camera_realtime = roate_x(x_angle) * roate_y(y_angle) * roate_z(z_angle) * camera;//实时相机
+		for (auto model : models) {
+			//渲染model
+			model=resize_model(model, last_resize);
+			for (std::size_t i = 0; i < model.size(); i++) {
+				Eigen::Matrix<double, 3, 1> dot = model[i];
+				Eigen::Matrix<double, 3, 1> dot1_realtime = camera_realtime.inverse() * world_base * dot;
+				for (std::size_t j = 0; j < model.size(); j++) {
+					Eigen::Matrix<double, 3, 1> dot = model[j];
+					Eigen::Matrix<double, 3, 1> dot2_realtime = camera_realtime.inverse() * world_base * dot;
+					Eigen::Matrix<double, 3, 1> window_dot1 = to_xy(dot1_realtime(0), dot1_realtime(1), dot1_realtime(2));
+					Eigen::Matrix<double, 3, 1> window_dot2 = to_xy(dot2_realtime(0), dot2_realtime(1), dot2_realtime(2));
+					draw_t_line(window_dot1, window_dot2);
+				}
+			}
+		}
+		ExMessage message;
+		if (peekmessage(&message)) {
+			if (message.lbutton) {//做鼠标按下
+				int dx = message.x - last_x;
+				int dy = message.y - last_y;
+				last_x = message.x;
+				last_y = message.y;
+				if (fabs(dx) < 10 && fabs(dy) < 10) {
+					x_angle =((int)x_angle+ dy)%360, y_angle =((int)y_angle+ dx)%360, z_angle += 0;
+				}
+			}
+			if (message.wheel) {
+				if(message.ctrl){//缩放
+					double dz = fabs(message.wheel) / 600.;//一次滚轮120
+					if (message.wheel < 0&&(last_resize - dz)>0.1) {//变小
+						last_resize = last_resize - dz;
+					}
+					else if(last_resize + dz<5) {//变大
+						last_resize = last_resize + dz;
 					}
 				}
-				Sleep(10);
-				//在屏幕中相机永远在屏幕中间，且相机坐标系屏幕右手水平方向为x，屏幕向里方向为z，屏幕向上方为y
-				cleardevice();
+				else {//z轴旋转
+					double dz = message.wheel / 10.;
+					z_angle = ((int)z_angle + (int)dz) % 360;
+				}
 			}
-	//	}
-	//}
-	std::cout << "NOW NOTHING RUN"<<count << std::endl;
-	_getch();
+			//画出文字
+			RECT r = { 0, 0, 639, 479 };
+			TCHAR s[512];
+			_stprintf_s(s, _T("x_angle %.3lf y_angle %.3lf z_angle %.3lf resize %.3lf "),x_angle,y_angle,z_angle,last_resize);
+			drawtext(s, & r, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+		}
+		flushmessage();
+		Sleep(20);
+		//在屏幕中相机永远在屏幕中间，且相机坐标系屏幕右手水平方向为x，屏幕向里方向为z，屏幕向上方为y
+		cleardevice();
+	}
 	return 0;
 }
